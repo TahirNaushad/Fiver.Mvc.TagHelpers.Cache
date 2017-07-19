b78c4ba4147bae36e5f32f05039025330a35837a
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-19 15:43:28 +0100

    Cache Tag Helper

---
+++ Fiver.Mvc.TagHelpers.Cache/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Mvc.TagHelpers.Cache.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
